f9596b984e81bdfbf57797313c4edf675e0a1874
Adrian Urbański <adrianu161999@gmail.com>
2022-06-08 13:31:17 +0200

    Add bert factory

--- thegreatknowledgeheist/models/bert.py
+++ thegreatknowledgeheist/models/bert.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Literal, Union
 
 import pytorch_lightning as pl
 import torch
@@ -7,6 +8,7 @@ from torch.optim.lr_scheduler import StepLR
 from torchmetrics import F1Score
 from torchmetrics.functional import accuracy
 from transformers import (
+    BertConfig,
     BertForMultipleChoice,
     BertForSequenceClassification,
     BertForTokenClassification,
@@ -76,39 +78,63 @@ class BaseBert(pl.LightningModule, ABC):
 
 
 class AmazonPolarityBert(BaseBert):
-    def __init__(self, config):
+    def __init__(self, bert_config: BertConfig, config, pretrained: bool = True,
+                 pretrained_name_or_path: Union[str, None] = None):
         super().__init__(config)
 
-        self.model = BertForSequenceClassification.from_pretrained(
-            "bert-base-uncased", num_labels=2
-        )
+
+        bert_config.num_labels = 2
+
+        if pretrained:
+            self.model = BertForSequenceClassification.from_pretrained(
+                pretrained_name_or_path, config=bert_config
+            )
+        else:
+            self.model = BertForSequenceClassification(bert_config)
+
         if config["freeze_first_n"] != -1:
             self.freeze_first_n(config["freeze_first_n"])
-        self.f1 = F1Score(num_classes=2, average="macro")
 
+        self.f1 = F1Score(num_classes=bert_config.num_labels, average="macro")
 
 class SwagBert(BaseBert):
-    def __init__(self, config):
+    def __init__(self, config, bert_config: BertConfig, pretrained: bool = True,
+                 pretrained_name_or_path: Union[str, None] = None):
         super().__init__(config)
 
-        self.model = BertForMultipleChoice.from_pretrained(
-            "bert-base-uncased", num_labels=4
-        )
+        bert_config.num_labels = 4
+
+        if pretrained:
+            self.model = BertForMultipleChoice.from_pretrained(
+                pretrained_name_or_path, config=bert_config
+            )
+        else:
+            self.model = BertForMultipleChoice(bert_config)
+
         if config["freeze_first_n"] != -1:
             self.freeze_first_n(config["freeze_first_n"])
-        self.f1 = F1Score(num_classes=4, average="macro")
+
+        self.f1 = F1Score(num_classes=bert_config.num_labels, average="macro")
+
 
 
 class AcronymIdentificationBert(BaseBert):
-    def __init__(self, config):
+    def __init__(self, config, bert_config: BertConfig, pretrained: bool, pretrained_name_or_path: str):
         super().__init__(config)
 
-        self.model = BertForTokenClassification.from_pretrained(
-            "bert-base-uncased", num_labels=5
-        )
+        bert_config.num_labels = 5
+
+        if pretrained:
+            self.model = BertForTokenClassification.from_pretrained(
+                pretrained_name_or_path, config=bert_config
+            )
+        else:
+            self.model = BertForTokenClassification(bert_config)
+
         if config["freeze_first_n"] != -1:
             self.freeze_first_n(config["freeze_first_n"])
-        self.f1 = F1Score(num_classes=5, average="macro")
+
+        self.f1 = F1Score(num_classes=bert_config.num_labels, average="macro")
 
     def calculate_accuracy(self, logits, labels):
         preds = torch.flatten(logits.argmax(-1))
@@ -119,3 +145,27 @@ class AcronymIdentificationBert(BaseBert):
         preds = torch.flatten(logits.argmax(-1))
         labels = torch.flatten(labels)
         return self.f1(preds, labels)
+
+
+TASK = Literal['amazon_polarity', 'acronym_identification', 'swag']
+
+
+class BertFactory:
+    def __init__(self):
+        self._model_builders = {
+            'amazon_polarity': AmazonPolarityBert,
+            'acronym_identification': AcronymIdentificationBert,
+            'swag': SwagBert,
+        }
+
+    def create_model(self, task_name: TASK, config, bert_config: BertConfig = None, pretrained: bool = True,
+                     pretrained_name_or_path: Union[str, None] = None):
+        if pretrained_name_or_path is None:
+            pretrained_name_or_path = "bert-base-uncased"
+
+        if bert_config is None:
+            bert_config = BertConfig.from_pretrained(pretrained_name_or_path)
+
+        model_builder = self._model_builders[task_name]
+
+        return model_builder(config, bert_config, pretrained, pretrained_name_or_path)
